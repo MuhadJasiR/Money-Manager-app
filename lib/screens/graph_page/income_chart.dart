67c4f74cbdb9f2ff99ae19f07394880e3941eb71
MuhadJasiR <alijasir051@gmail.com>
2023-01-29 00:22:51 +0530

    added_graph

--- lib/screens/graph_page/income_chart.dart
+++ lib/screens/graph_page/income_chart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:money_manager_app/db/transacrtion_model.dart';
 import 'package:money_manager_app/db/transaction_db.dart';
+import 'package:money_manager_app/models/category_modal.dart';
+import 'package:money_manager_app/screens/finalcial_report.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Income_chart extends StatefulWidget {
@@ -13,23 +15,38 @@ class Income_chart extends StatefulWidget {
 class _Income_chartState extends State<Income_chart> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 400,
-        width: 400,
-        child: SfCircularChart(
-          legend: Legend(
-              isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
-          series: <CircularSeries>[
-            DoughnutSeries<TransactionModel, String>(
-                dataSource:
-                    TransactionDB.instance.transactionListNotifier.value,
-                xValueMapper: (TransactionModel data, _) => data.category.name,
-                yValueMapper: (TransactionModel data, _) => data.amount,
-                dataLabelSettings: DataLabelSettings(isVisible: true)),
-          ],
-        ),
-      ),
-    );
+    return ValueListenableBuilder(
+        valueListenable: doughnutChartNotifier,
+        builder: ((context, value, _) {
+          var allIncomeData = value
+              .where((element) => element.category.type == CategoryType.income)
+              .toList();
+          return allIncomeData.isEmpty
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("Nothing Found")],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SfCircularChart(
+                      legend: Legend(
+                          isVisible: true,
+                          overflowMode: LegendItemOverflowMode.wrap),
+                      series: <CircularSeries>[
+                        DoughnutSeries<TransactionModel, String>(
+                            // innerRadius: "105",
+                            dataSource: allIncomeData,
+                            xValueMapper: (TransactionModel data, _) =>
+                                data.category.name,
+                            yValueMapper: (TransactionModel data, _) =>
+                                data.amount,
+                            dataLabelSettings:
+                                DataLabelSettings(isVisible: true)),
+                      ],
+                    ),
+                  ],
+                );
+        }));
   }
 }
